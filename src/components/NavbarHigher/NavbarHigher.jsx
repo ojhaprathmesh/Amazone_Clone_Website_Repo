26851a39bd9ae0b5914f65acc587370205b2033c
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-08-06 16:24:31 +0530

    Changed import method of assets.jsx (Default->Named)

--- src/components/NavbarHigher/NavbarHigher.jsx
+++ src/components/NavbarHigher/NavbarHigher.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import assets from "../../assets/assets";
+import { assets } from "../../assets/assets";
 import LanguageSelectionModal from "../Modals/LanguageSelectionModal";
 import useCountryCode from "../Hooks/LocationHook";
 import useLanguage from "../Hooks/LanguageHook";
